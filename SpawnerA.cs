e32f51206a9ab3ddeb6728a5e384737f3bd8f855
Emil <46216287+LoGinto@users.noreply.github.com>
2019-06-02 21:24:05 +0400

    Update SpawnerA.cs

--- SpawnerA.cs
+++ SpawnerA.cs
@@ -1,17 +1,17 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//simple enemy spawner
 public class SpawnerA : MonoBehaviour
 {
-    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject enemyPrefab;//Which enemy we will spawn
     bool spawned = true;
    // [SerializeField] bool looping = false;
     [SerializeField] float timebetweenspawns = 2f;
     [Range(1,50)]
     [SerializeField]
-    int enemyCountMax = 1;
+    int enemyCountMax = 1;//variable used in a loop
 
 
 
@@ -22,24 +22,24 @@ public class SpawnerA : MonoBehaviour
     {
         do
         {
-            yield return StartCoroutine(CreateSpawn());
+            yield return StartCoroutine(CreateSpawn());//coroutine of spawning
         }
-        while (spawned);
+        while (spawned);//we can say that it's infinite loop for now
     }
 
-    private IEnumerator CreateSpawn()
+    private IEnumerator CreateSpawn()//instantiating a spawn
     {
-        for(int enemyCount = 0; enemyCount < enemyCountMax;enemyCount++)
+        for(int enemyCount = 0; enemyCount < enemyCountMax;enemyCount++)//loop which determines max amount of enemy
         {
             var newEnemy = Instantiate(FindObjectOfType<SpawnerA>().GetEnemyPrefab(),
-                transform.position, Quaternion.identity);
-            yield return new WaitForSeconds(timebetweenspawns);
+                transform.position, Quaternion.identity);//instantiate a prefab on a game object transform
+            yield return new WaitForSeconds(timebetweenspawns);//wait 
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //nothing here
     }
 }

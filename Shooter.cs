19756b72c4af4f58c684e3ddeaec88b4813ec403
Emil <46216287+LoGinto@users.noreply.github.com>
2019-06-30 16:28:34 +0400

    Add files via upload

---
+++ Shooter.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+
+    [SerializeField] GameObject projectile, gun;
+    AttackerSpawner myLaneSpawner;//attacker spawner component
+    Animator animator;
+
+    private void Start()
+    {
+        SetLaneSpawner();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(IsAttackerInLane())
+        {
+            animator.SetBool("isAttacking", true);//changing animator state
+        }
+        else
+        {
+            animator.SetBool("isAttacking", false);//changing animator state
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner spawner in spawners)
+        {
+            bool IsCloseEnough = 
+                (Mathf.Abs(spawner.transform.position.y - transform.position.y) 
+                <= Mathf.Epsilon);//check  whether enemy is on y coordinaate
+            if (IsCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public void Fire()
+    {
+        Instantiate(projectile, gun.transform.position, transform.rotation);
+    }
+
+}

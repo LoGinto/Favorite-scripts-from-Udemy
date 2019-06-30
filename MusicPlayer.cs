7c66552e4d4d083f094f7be84118990b45a145c8
Emil <46216287+LoGinto@users.noreply.github.com>
2019-06-30 16:00:46 +0400

    Add files via upload

---
+++ MusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);//music through the game 
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = PlayerPrefsController.GetMasterVolume();//Master volume float = volume
+    }
+    public void SetVolume(float volume)
+    {
+        audioSource.volume = volume;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ OptionsController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using UnityEngine.SceneManagement;
+public class OptionsController : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] float defaultVolume = 0.8f;
+    [SerializeField] Slider difficultySlider;
+    [SerializeField] float defaultDifficulty = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        volumeSlider.value = PlayerPrefsController.GetMasterVolume();//accesing the slider value
+        //and assigning it to prefs
+        difficultySlider.value = PlayerPrefsController.GetDifficulty();//same here with difficulty
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var musicPlayer = FindObjectOfType<MusicPlayer>();
+        if (musicPlayer)
+        {
+            musicPlayer.SetVolume(volumeSlider.value);//corresponding to slider
+        }
+        else
+        {
+            return;
+            Debug.LogWarning("Start from splash screen, there is no music player here");
+
+        }
+
+    }
+   public void SaveAndExit()
+    {
+        PlayerPrefsController.SetMasterVolume(volumeSlider.value);
+        PlayerPrefsController.SetDifficulty(difficultySlider.value);
+        FindObjectOfType<LevelLoader>().LoadMainMenu();
+    }
+    public void SetDefault()
+    {
+        volumeSlider.value = defaultVolume;
+        difficultySlider.value = defaultDifficulty;
+    }
+  
+}

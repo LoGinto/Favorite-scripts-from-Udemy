fe4583625a7bbbafc08525131a099fcab6dea072
Emil <46216287+LoGinto@users.noreply.github.com>
2019-07-17 16:57:34 +0400

    Add files via upload

---
+++ PlayerMovement2D.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    // Config 
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
+
+    // State
+    bool isAlive = true;
+
+    // References, initiation 
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    Collider2D myCollider2D;
+    float gravityScaleAtStart;
+
+    // Getting components 
+	void Start () {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myCollider2D = GetComponent<Collider2D>();
+        gravityScaleAtStart = myRigidBody.gravityScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Run();
+        ClimbLadder();
+        Jump();
+        FlipSprite();
+	}
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // value is betweeen -1 to +1
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);//new vector which equals x input * speed
+        myRigidBody.velocity = playerVelocity;//assigning vector
+      
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;//if there is a speed
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);//change animation state
+    }
+
+    private void ClimbLadder()
+    {
+        if(!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing")))//getting the layer mask
+        {
+            myAnimator.SetBool("Climbing", false);//change animation state
+            myRigidBody.gravityScale = gravityScaleAtStart;
+            return;
+        }
+
+        float controlThrow = CrossPlatformInputManager.GetAxis("Vertical");//vertical -1 to +1
+        Vector2 climbVelocity = new Vector2(myRigidBody.velocity.x, controlThrow * climbSpeed);//vector with y direction
+        myRigidBody.velocity = climbVelocity;//assigning vector to velocity 
+        myRigidBody.gravityScale = 0f;
+
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("Climbing", playerHasVerticalSpeed);//change animation state
+
+    }
+
+    private void Jump()
+    {
+        if(!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }//if not touching ground
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))//if the key is pressed
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);//addition to y speed
+            myRigidBody.velocity += jumpVelocityToAdd;
+        }
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;//if there is movement
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);//change scale to 1 or -1
+        }
+    }
+
+}
